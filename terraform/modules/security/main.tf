6321d19117de64203658922722bc6f6907da5eb8
Isaiah Wright <isaiah.wright@tylertech.com>
2025-05-01 15:18:51 -0400

    removed old security config

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -1,27 +1,3 @@
-# resource "aws_security_group" "web" {
-#   name = "${var.project_name}-sg"
-#   description = "Allow inbound SSH and all traffic"
-#   vpc_id = var.vpc_id
-
-#   ingress  {
-#     from_port = 0
-#     to_port = 65535
-#     protocol = "tcp"
-#     cidr_blocks = [var.ingress_cidr]
-#   }
-
-#   egress  {
-#     from_port = 0
-#     to_port = 0
-#     protocol = "-1"
-#     cidr_blocks = [var.egress_cidr]
-#   }
-
-#   tags = {
-#     Name = "${var.project_name}-sg"
-#   }
-# }
-
 resource "aws_security_group" "web" {
   name = "${var.project_name}-sg"
   description = "Allow inbound SSH and all traffic"
